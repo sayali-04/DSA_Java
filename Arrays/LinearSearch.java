23739e9b1903e2b83a7a9f13f397947d52a7f54e
Sayali Pawar <94274989+sayali-04@users.noreply.github.com>
2026-08-01 22:00:08 +0530

    Update LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,5 +1,26 @@
 package Arrays;
-
 public class LinearSearch {
-    
+
+    public static int lineardemo(int numbers[],int key){
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,12,16,18};
+        int key=10;
+        
+        int index=lineardemo(numbers, key);
+        if(index==-1){
+            System.out.println("Key not found");
+        }
+        else{
+            System.out.println("Key found at index:"+index);
+        }
+    }
 }
+
+
